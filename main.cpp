8c7bad8f576bcec64ac99fdc8392da279c52bdc3
Nicholas Qualls <nicholaseqboss@gmail.com>
2022-12-11 19:18:54 -0600

    Add Sprite Class

--- main.cpp
+++ main.cpp
@@ -2,6 +2,26 @@
 const int screenWidth = 640;
 const int screenHeight = 480;
 
+class Sprite {
+	int X;
+	int Y;
+	Texture2d Texture;
+
+	int SizeX;
+	int SizeY;
+
+	void display(){
+		DrawTexture(texture,X,Y,WHITE);
+	}
+	void drawHitbox(Color tint){
+		DrawRectangle(X,Y,SizeX,SizeY,tint);
+	}
+	Rectangle getHitbox(){
+
+	}
+
+}
+
 int main(){
 	InitWindow(screenHeight,screenWidth,"AA");
 	SetTargetFPS(60);
@@ -10,9 +30,9 @@ int main(){
 	while (!WindowShouldClose())    // Detect window close button or ESC key
 	{
 		BeginDrawing();
-			ClearBackground(BLACK);
-			DrawText("Press Alt + Enter to Toggle full screen!", 190, 200, 20, LIGHTGRAY);
+		ClearBackground(BLACK);
 		EndDrawing();
 	}
 	CloseWindow();
 }
+
