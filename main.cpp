d13626a37b831b182267cd8c68220d204844c799
Nicholas Qualls <nicholaseqboss@gmail.com>
2022-12-11 19:47:48 -0600

    Player move

--- main.cpp
+++ main.cpp
@@ -1,15 +1,16 @@
 #include "raylib.h"
 const int screenWidth = 640;
 const int screenHeight = 480;
+const int playerSpeed = 3;
 
 class Sprite {
+	int X;
+	int Y;
+	Texture2D Texture;
 
 	int SizeX;
 	int SizeY;
 public:
-	int X;
-	int Y;
-	Texture2D Texture;
 	void display(){
 		DrawTexture(Texture,X,Y,WHITE);
 	}
@@ -19,29 +20,55 @@ public:
 	Rectangle* getHitbox(){
 		return nullptr;
 	}
-	Sprite(Texture2D Texture, int X, int Y){
+	Sprite(Texture2D Texture, int X, int Y, int SizeX, int SizeY){
 		this->X = X;
 		this->Y = Y;
+		this->SizeX = SizeX;
+		this->SizeY = SizeY;
 		this->Texture = Texture;
+	}
+	void moveX(int move){
+		int Xmax = screenWidth-SizeX;
+		int Xmin = 0;
+		move+=X;
+		if(move>=Xmax){
+			move=Xmax;
+		}
+		if(move<Xmin){
+			move=Xmin;
+		}
+		X=move;
+	}
+	void moveY(int move){
+		int Ymax = screenHeight-SizeY;
+		int Ymin = 0;
+		move+=Y;
+		if(move>=Ymax){
+			move=Ymax;
+		}
+		if(move<Ymin){
+			move=Ymin;
+		}
+		Y=move;
 	}
 };
 
-void movePlayer(Sprite*player){
-	if(IsKeyPressed(KEY_RIGHT))
-		player->X++;
-	if(IsKeyPressed(KEY_LEFT))
-		player->X--;
-	if(IsKeyPressed(KEY_UP))
-		player->Y--;
-	if(IsKeyPressed(KEY_DOWN))
-		player->Y++;
+void movePlayer(Sprite *player){
+	if(IsKeyDown(KEY_RIGHT))
+		player->moveX(playerSpeed);
+	if(IsKeyDown(KEY_LEFT))
+		player->moveX(-playerSpeed);
+	if(IsKeyDown(KEY_UP))
+		player->moveY(-playerSpeed);
+	if(IsKeyDown(KEY_DOWN))
+		player->moveY(playerSpeed);
 }
 int main(){
 	InitWindow(screenHeight,screenWidth,"AA");
 	SetTargetFPS(60);
 	int display = GetCurrentMonitor();
 	ToggleFullscreen();
-	Sprite player(LoadTexture("player.png"),240,240);
+	Sprite player(LoadTexture("player.png"),240,240,32,32);
 	
 	while (!WindowShouldClose())    // Detect window close button or ESC key
 	{
