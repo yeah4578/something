d2b5b52ce5e76048db463bc6ee08c11916f9aa3f
Nicholas Qualls <nicholaseqboss@gmail.com>
2022-12-11 18:50:45 -0600

    Initialize Window

--- main.cpp
+++ main.cpp
@@ -1,3 +1,18 @@
 #include "raylib.h"
 const int screenWidth = 640;
 const int screenHeight = 480;
+
+int main(){
+	InitWindow(screenHeight,screenWidth,"AA");
+	SetTargetFPS(60);
+	int display = GetCurrentMonitor();
+	ToggleFullscreen();
+	while (!WindowShouldClose())    // Detect window close button or ESC key
+	{
+		BeginDrawing();
+			ClearBackground(BLACK);
+			DrawText("Press Alt + Enter to Toggle full screen!", 190, 200, 20, LIGHTGRAY);
+		EndDrawing();
+	}
+	CloseWindow();
+}
